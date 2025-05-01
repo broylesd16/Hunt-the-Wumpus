570576d0ea58668f30823fe30c5da33c184e1a57
David Broyles <broylesd16@mycu.concord.edu>
2025-05-01 11:23:51 -0400

    Code from the python conway's game of life that I wrote that makes a grid

--- wumpus.py
+++ wumpus.py
@@ -1,1 +1,20 @@
-print("Test")
+import random
+#import pygame
+#import sys
+
+
+def main():
+    rows = 20
+    columns = 20
+
+    grid = []
+    for i in range(rows):
+        grid.append(["-"] * columns)
+
+    for i in range(0, rows):
+        for j in range(0, columns):
+            ran = random.randint(0, 1)
+            if ran == 0:
+                grid[i][j] = "-"
+            elif ran == 1:
+                grid[i][j] = "#"
